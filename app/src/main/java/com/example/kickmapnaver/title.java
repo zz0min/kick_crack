582c40b04f84b97493be797cd2e363145f241dec
jininsong <songjinin0214@naver.com>
2024-06-01 20:17:59 +0900

    알림 기능 추가 필요

--- app/src/main/java/com/example/kickmapnaver/title.java
+++ app/src/main/java/com/example/kickmapnaver/title.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Title extends AppCompatActivity {
+public class title extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +16,9 @@ public class Title extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Title.this, MainActivity.class);
+                Intent intent = new Intent(title.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         }, 1000); // 5000 밀리초 = 5초
     }
